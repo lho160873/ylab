06b2cdde48b1b99e63119ffce756cce482e2f776
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-04-12 18:05:21 +0300

    +css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
+
 
 
