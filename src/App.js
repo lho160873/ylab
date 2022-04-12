0eaf15b0ccf58fa57feace804bfce428295feb6c
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-04-12 10:54:31 +0300

    add proj

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Привет мир!
+      <ul>
+        <li>Меню 1</li>
+        <li>Меню 2</li>
+        <li>Меню 3</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
