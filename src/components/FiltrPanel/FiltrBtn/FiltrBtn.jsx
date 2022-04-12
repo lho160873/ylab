71a2fc12303aeb3a47bda95dd9089ab415363d45
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-04-12 18:39:03 +0300

    +btn

--- src/components/FiltrPanel/FiltrBtn/FiltrBtn.jsx
+++ src/components/FiltrPanel/FiltrBtn/FiltrBtn.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import s from './FiltrBtn.module.css';
 
+function getService() {
+  /*const myList = document.querySelector('tbody');
+  fetch('http://89.108.65.107:8006/api/v1/service/')
+  .then(response => response.json())
+  .then(data => {
+    for (const d of data) {
+      let tr = document.createElement('tr');
+      
+      let td_name = document.createElement('td');        
+      td_name.innerHTML = '<strong>' + d.name + '</strong>';
+      tr.appendChild(td_name);
+      
+     
+    
+      myList.appendChild(tr);
+    }
+  }
+  );*/
+  alert("OK");
+}
+
 const FiltrBtn = () => {
   return (
-    <div className={s.filtrBtn}>
+    <div className={s.filtrBtn} type="button" onClick={getService}>
       Фильтры
     </div>
   );
 }
+
+
+
+
+
 
 export default FiltrBtn;
