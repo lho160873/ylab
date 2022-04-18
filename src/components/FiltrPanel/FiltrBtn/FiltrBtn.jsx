098b676daac89f158073e5ca0a12eeac6484166e
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-04-18 12:18:15 +0300

    выравнивание иконок

--- src/components/FiltrPanel/FiltrBtn/FiltrBtn.jsx
+++ src/components/FiltrPanel/FiltrBtn/FiltrBtn.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import s from './FiltrBtn.module.css';
-import Icons from '../../Icons/Icons';
-
+import React from "react";
+import s from "./FiltrBtn.module.css";
+import Icons from "../../Icons/Icons";
 
 const FiltrBtn = (props) => {
-
   let btnRef = React.createRef();
 
   let addFiltr = () => {
-    props.addFiltr(btnRef.current.id);   
-  }
+    props.addFiltr(btnRef.current.id);
+  };
 
   if (props.isActive) {
     return (
-      <div className={s.filtrBtnCheck} type="button" onClick={addFiltr} ref={btnRef} id={props.id} name={props.name}>       
-           <Icons id={props.id} name={props.name} icon={props.icon}/>
-          {props.caption}
+      <div
+        className={`${s.filtrBtn} ${s.filtrBtnCheck}`}
+        type="button"
+        onClick={addFiltr}
+        ref={btnRef}
+        id={props.id}
+        name={props.name}
+      >
+        <Icons id={props.id} name={props.name} icon={props.icon} />
+        <span>{props.caption}</span>
       </div>
     );
-  }
-  else return (
-    <div className={s.filtrBtn} type="button" onClick={addFiltr} ref={btnRef} id={props.id} name={props.name}>
-         <Icons id={props.id} name={props.name} icon={props.icon}/>
-        {props.caption}
-    </div>
-  );
-
-}
-
-
-
-
-
+  } else
+    return (
+      <div
+        className={s.filtrBtn}
+        type="button"
+        onClick={addFiltr}
+        ref={btnRef}
+        id={props.id}
+        name={props.name}
+      >
+        <Icons id={props.id} name={props.name} icon={props.icon} />
+        <span>{props.caption}</span>
+      </div>
+    );
+};
 
 export default FiltrBtn;
--- src/components/FiltrPanel/FiltrPanel.jsx
+++ src/components/FiltrPanel/FiltrPanel.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import s from './FiltrPanel.module.css';
-import FiltrBtn from './FiltrBtn/FiltrBtn'
+import React from "react";
+import s from "./FiltrPanel.module.css";
+import FiltrBtn from "./FiltrBtn/FiltrBtn";
 
 const createBtn = (props) => {
-   return  props.state.filtrs.map( p => <FiltrBtn key={p.id} caption={p.caption} id={p.id} name={p.name} isActive={p.isActive} icon={p.icon} addFiltr={props.addFiltr}/>);   
-}
+  return props.state.filtrs.map((p) => (
+    <FiltrBtn
+      key={p.id}
+      caption={p.caption}
+      id={p.id}
+      name={p.name}
+      isActive={p.isActive}
+      icon={p.icon}
+      addFiltr={props.addFiltr}
+    />
+  ));
+};
 
 const FiltrPanel = (props) => {
   return (
     <div className={s.filtrPanel}>
-      <div className={s.filtrpanelCaption}>
-        Инструменты
-      </div>
-      <div className={s.filtrpanelBtn}>      
-      { createBtn(props) }
-      </div>
+      <div className={s.filtrpanelCaption}>Инструменты</div>
+      <div className={s.filtrpanelBtn}>{createBtn(props)}</div>
     </div>
-
   );
-}
+};
 
 export default FiltrPanel;
