d677baaa23b8581ccf7550d8bb4aacb30de6c801
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-04-18 13:42:11 +0300

    Теперь ссылка работает на весь блок инструмента

--- src/components/ServicePanel/ServiceItem/ServiceItem.jsx
+++ src/components/ServicePanel/ServiceItem/ServiceItem.jsx
@@ -2,19 +2,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './ServiceItem.module.css';
 import Icons from '../../Icons/Icons';
+import star from '../../../icons/star.svg';
+import Vector from '../../../icons/Vector.svg';
+import Vector2 from '../../../icons/Vector 2.svg';
 
 
 const ServiceItem = (props) => {
   let itemRef = React.createRef();
   let path = "/servicepage/" + props.name.replace(/\s+/g, '').toLowerCase();
+  let addFavorite = () => {
+    //props.addFiltr(btnRef.current.id);
+    console.log("addFavorite");
+  };
+  /*
+        <img src={star} className={s.iconSvg} alt="Избанное" onClick={addFavorite} />  
+        <img src={Vector} className={s.iconSvg} alt="Избанное" />
+        <img src={Vector2} className={s.iconSvg} alt="Избанное" />
+        <div className={s.iconFavotite} onClick={addFavorite} />
+        <div className={s.iconFavotiteActive} onClick={addFavorite} />*/
+
   return (
-    <div className={s.serviceItem} >
-      <Icons id={props.id} name={props.name} icon={props.icon} />
-      <NavLink to={path} ref={itemRef} id={props.id}>
+    <NavLink to={path} ref={itemRef} id={props.id} className={s.serviceItem} >
+      <div>
+        <Icons id={props.id} name={props.name} icon={props.icon} />
         <h2>{props.name}</h2>
         <br />{props.description}
-      </NavLink>
-    </div>
+      </div>
+    </NavLink>
+
   );
 }
 
