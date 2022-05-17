ad650eda1644a7e6a00fbc85c83fff279e05eb58
lho160873 <51647265+lho160873@users.noreply.github.com>
2022-05-17 16:25:08 +0300

    +

---
+++ src/api/api.js
@@ -0,0 +1,37 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "5a68fee6-ea86-454c-8c77-eefc235eada1"
+    }
+});
+
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    },
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+    },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId);
+    }
+}
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    }
+}
+
+
